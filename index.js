117f12b8bcde16ce1bfce89e50e5e28f7d4edde6
Adam Cowley <adam@adamcowley.co.uk>
2017-08-03 13:01:13 +0100

    Added a few comments

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// Define a Neode Instance
 const neode = require("neode")
+
+    // Using configuration from .env file
     .fromEnv()
+
+    // Including the models in the models/ directory
     .with({
         Movie: require("./models/Movie"),
         Person: require("./models/Person"),
@@ -7,6 +12,9 @@ const neode = require("neode")
         Director: require("./models/Director")
     });
 
+
+// Merge a movie on the title field and add additional properties
+// Neode will also work out what to merge on based on the schema definition using `neode.merge`
 neode.model("Movie").mergeOn({
     title: "Adulthood",
 }, {
@@ -14,18 +22,17 @@ neode.model("Movie").mergeOn({
     tagline: "Are you dizzy blud?"
 })
 .then(adulthood => {
+    // Get the 'Actor' definition
     const actor = neode.model("Actor");
 
+    // Create some 'Actor' nodes
     return Promise.all([
         actor.mergeOn({name: "Noel Clarke"}),
         actor.mergeOn({name: "Scarlett Alice Johnson"}),
         actor.mergeOn({name: "Adam Deacon"})
     ])
-    .then(res => {
-        const noel = res[0];
-        const scarlett = res[1];
-        const adam = res[2];
-
+    .then(([noel, scarlett, adam]) => {
+        // Relate the actors to the movie
         return Promise.all([
             noel.relateTo(adulthood, "acts_in", {name: "Sam"}),
             scarlett.relateTo(adulthood, "acts_in", {name: "Lexi"}),
@@ -37,8 +44,11 @@ neode.model("Movie").mergeOn({
     });
 })
 .then(adulthood => {
+    // Merge a 'Director' node based on the name
     return neode.merge("Director", {
         name: "Noel Clarke",
+        // Neode will create relationships when either a Node instance,
+        // ID property or Object containing match parameters is passed
         directed: [
             adulthood
         ]
@@ -48,14 +58,15 @@ neode.model("Movie").mergeOn({
     });
 })
 .then(adulthood => {
-    console.log('Created ', adulthood.id(), adulthood.properties());
+    // Output some results
+    console.log('Created '#, adulthood.id(), adulthood.properties());
 })
 .catch(e => {
     console.log("Error :(", e, e.details);
 })
 .then(() => {
+    // Close all connections to the graph
     neode.close();
 
     console.log('Shutdown');
 });
-/**/
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+/**
+ * A basic example using Express to create a Rest API
+ */
 const express = require('express');
-const neode = require('neode').fromEnv().with({Movie: require('./models/Movie')});
+const neode = require('neode')
+            .fromEnv()
+            .with({Movie: require('./models/Movie')});
 
 const app = express();
 
+/**
+ * Use neode.all() to return a paginated list of movies
+ */
 app.get('/movies', (req, res) => {
     const order_by = req.query.order || 'title';
     const sort = req.query.sort || 'ASC';
@@ -15,6 +23,8 @@ app.get('/movies', (req, res) => {
 
     neode.all('Movie', params, order , limit, skip)
         .then(res => {
+            // `all` returns a NodeCollection - this has a toJson method that
+            // will convert all Nodes within the collection into a JSON object
             return res.toJson();
         })
         .then(json => {
@@ -25,6 +35,9 @@ app.get('/movies', (req, res) => {
         });
 });
 
+/**
+ * Use `findById` to get a movie by it's internal Node ID
+ */
 app.get('/movies/~:id', (req, res) => {
     neode.findById('Movie', parseInt(req.params.id))
         .then(res => {
@@ -38,6 +51,9 @@ app.get('/movies/~:id', (req, res) => {
         });
 });
 
+/**
+ * Use `find` to get a moivie by it's primary key
+ */
 app.get('/movies/:id', (req, res) => {
     neode.find('Movie', req.params.id)
         .then(res => {
@@ -51,7 +67,10 @@ app.get('/movies/:id', (req, res) => {
         });
 });
 
+/**
+ * Listen on port 3000
+ */
 app.listen(3000, function () {
-  console.log('app listening on :3000');
+  console.log('app listening on http://localhost:3000');
 });
 
